c8b1fc56b445bac4dc0415238425b751e4d5f61d
Piotr Wilk <piotrwilk@interia.pl>
2018-05-31 19:27:24 +0200

    some improvment with loader in article component

--- src/components/Articles/SingleArticle.jsx
+++ src/components/Articles/SingleArticle.jsx
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react';
-import Loader from '../Loader';
+import React, { Fragment } from 'react'
 
 const SingleArticle = (props) => {
     if (!props.loading && props.id === 0) {
@@ -14,7 +13,24 @@ const SingleArticle = (props) => {
             </Fragment>
         )
     }
-    return (<Loader/>)
+    return (<div>
+            <div className="sk-circle">
+            <div className="sk-circle1 sk-child"></div>
+            <div className="sk-circle2 sk-child"></div>
+            <div className="sk-circle3 sk-child"></div>
+            <div className="sk-circle4 sk-child"></div>
+            <div className="sk-circle5 sk-child"></div>
+            <div className="sk-circle6 sk-child"></div>
+            <div className="sk-circle7 sk-child"></div>
+            <div className="sk-circle8 sk-child"></div>
+            <div className="sk-circle9 sk-child"></div>
+            <div className="sk-circle10 sk-child"></div>
+            <div className="sk-circle11 sk-child"></div>
+            <div className="sk-circle12 sk-child"></div>
+            <div className="loading-text">Loading</div>
+            </div> 
+        </div>
+    )
 
 }
 
