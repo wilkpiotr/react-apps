0e068971b478292d3cdb1433dadb222855c3340d
Piotr Wilk <piotrwilk@interia.pl>
2018-05-31 20:22:16 +0200

    add info 'no more image' and change btton value

--- src/components/MoreImages/Button.jsx
+++ src/components/MoreImages/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Button = ( {onClick, loading} ) => {
-    if (!loading)  {  
-        return (<div onClick={onClick}>
-            <a className="button is-large">Next Images</a>
+const Button = (props) => {
+    if (!props.loading)  {  
+        return (<div onClick={props.onClick}>
+            <a className="button is-large" disabled={props.disabled}>{props.value}</a>
         </div>)
     }
-    return (<div onClick={onClick}>
+    return (<div >
         <a className="button is-loading is-large">Loading</a>
     </div>)
 }
--- src/components/MoreImages/MoreImages.jsx
+++ src/components/MoreImages/MoreImages.jsx
@@ -9,13 +9,23 @@ class MoreImages extends Component {
 
   state = { images: null,
     page: 1,
-    loading: false}
+    loading: false,
+    disabled: false,
+    buttonValue: 'Next Images'}
 
   handleAddImages = () => {
+    console.log()
     this.setState({loading: true})
     axios
     .get(`http://localhost:3000/images?_page=${this.state.page + 1}&_limit=4`)
     .then((result) => {
+      console.log(result.data[0])
+      if (!result.data[0]) {
+        this.setState((state) => {
+          return {disabled: true,
+            buttonValue: 'No More Images'}
+        })
+      }
       const newImages = [...this.state.images, ...result.data];
       this.setState((state) => {
         return {images: newImages,
@@ -36,7 +46,7 @@ class MoreImages extends Component {
       <h1 className="title">More Images React App</h1>
       {/* First component mount should fetch data and show the loader */}
       <Images images={this.state.images}/>
-      <Button onClick={this.handleAddImages} loading={this.state.loading}/>
+      <Button onClick={this.handleAddImages} loading={this.state.loading} value={this.state.buttonValue} disabled={this.state.disabled}/>
     </div>
     </section>)
 
